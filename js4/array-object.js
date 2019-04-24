afa228fd866e3e6546b734e5a79682ffb6ce9c0d
Sukhveer Jawandha <sjawandha2@mail.greenriver.edu>
2019-04-24 17:50:46 -0400

    Added the function to display the output onweb page

--- js4/array-object.js
+++ js4/array-object.js
@@ -1,3 +1,9 @@
+/*
+ *Sukhveer Jawandha
+* 4/24/2019
+* 328/javascript/js4/array-object.js
+* This file contains function that separates the the values according to the object.
+ */
 function arrayToObj(aparam) {
     let arrayObject =
         {
@@ -26,3 +32,15 @@ var input = [-1, 5, "cat", false, 10.2, true, "dog"];
 var object = arrayToObj(input);
 
 
+function element(par)
+{
+    for (var key in par) {
+        var object = document.createElement("h2");
+        var value = document.createElement("p")
+        object.innerText= key.toUpperCase();
+        value.innerText= par[key].toString();
+        document.body.appendChild(object);
+        document.body.appendChild(value);
+    }
+}
+element(object);
